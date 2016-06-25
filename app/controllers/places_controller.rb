1fd69a0f4ef63da03bfd46e3a2cfffc4f33f28d7
Alireza <alimrb@yahoo.com>
2016-06-25 17:44:27 +0200

    adding submit form link in main page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,21 @@
 class PlacesController < ApplicationController
   def index 
-  	@places = Place.all.paginate(:page => params[:page], :per_page => 3)
+  	@places = Place.all.order("id DESC").paginate(page: params[:page], per_page: 3)
   end
 
   def new
     @place = Place.new
   end
   
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
